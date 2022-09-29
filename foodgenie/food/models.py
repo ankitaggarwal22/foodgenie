9e47f2124f550f57b1a3eade28b23c86f6e9ea92
ankitaggarwal22 <ankit.aggarwal22@hotmail.com>
2022-09-30 00:41:53 +0530

    checkin

--- foodgenie/food/models.py
+++ foodgenie/food/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    item_name=models.CharField(max_length=255, null=False, blank=False)
+    item_desc=models.CharField(max_length=255, null=False, blank=False)
+    item_price=models.IntegerField()
+    item_image=models.CharField(max_length=500, default="https://images.ctfassets.net/84wm3hhxw4gx/0sxerdVddcgpnd69VcMsx/414cb6a014fc90e5d96e07fef8022ccf/foodplaceholder.png")
+
+    def __str__(self) -> str:
+        return self.item_name
--- foodgenie/food/views.py
+++ foodgenie/food/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . import models
+from . import forms
 
 # Create your views here.
+def index(request):
+    item_list=models.Item.objects.all()
+    context={
+        'item_list':item_list,
+    }
+    return render(request, 'index.html', context)
+
+def detail(request, item_id):
+    item=models.Item.objects.get(id=item_id)
+    context={
+        'item':item,
+    }
+    return render(request, 'detail.html', context)
+
+def add_item(request):
+    form=forms.ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'add_item.html', {'form':form})
+
+def edit_item(request, id):
+    item = models.Item.objects.get(id=id)
+    form = forms.ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'add_item.html', {'form':form, 'item':item})
+
+def delete_item(request, id):
+    item = models.Item.objects.get(id=id)
+
+    if request.method=='POST':
+        item.delete()
+        return redirect('food:index')
+
+    return render(request, 'item_delete.html', {'item':item})
+
